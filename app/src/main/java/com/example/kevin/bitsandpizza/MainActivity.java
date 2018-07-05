77854a892098cfcf570f870d542269816077df13
kevinfivefive <kevin.bajaj@gmail.com>
2018-07-05 15:39:59 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/kevin/bitsandpizza/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kevin.bitsandpizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTitle("Bits and Pizzas");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
