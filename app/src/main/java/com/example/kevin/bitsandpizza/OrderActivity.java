0762d7fa6264536cbb31b8ff8f874ccb70745b87
kevinfivefive <kevin.bajaj@gmail.com>
2018-07-06 14:57:48 -0500

    Add Create Order, Share, and Up buttons to toolbar

---
+++ app/src/main/java/com/example/kevin/bitsandpizza/OrderActivity.java
@@ -0,0 +1,29 @@
+package com.example.kevin.bitsandpizza;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
